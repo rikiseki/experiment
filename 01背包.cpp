72ef7366152371fb9fba5118d110cbdb578a7956
KIDstark <51429420+KIDstark@users.noreply.github.com>
2020-08-08 13:38:06 +0800

    算法

---
+++ 01背包.cpp
@@ -0,0 +1,145 @@
+﻿// 01背包.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+//int max(int a, int b)
+//{
+//	if (a >= b)
+//		return a;
+//	else
+//		return b;
+//}
+//void package(int* v, int* w, int c, int n, int** m)
+//{
+//	for (int j = 0; j <= c; j++)
+//	{
+//		if (j < w[n])
+//			m[n][j] = 0;
+//		else
+//			m[n][j] = v[n];
+//	}
+//	for (int i = n - 1; i >= 1; i--)
+//	{
+//		for (int j = 0; j <= c; j++)
+//		{
+//			if (w[i] > j)
+//				m[i][j] = m[i + 1][j];
+//			else
+//				m[i][j] = max(m[i + 1][j], v[i] + m[i + 1][j - w[i]]);
+//		}
+//	}
+//}
+//void Traceback(int** m, int* w, int c, int n, int* x)
+//{
+//	for (int i = 1; i < n; i++)
+//	{
+//		if (m[i][c] == m[i + 1][c])
+//			x[i] = 0;
+//		else
+//		{
+//			x[i] = 1;
+//			c -= w[i];
+//		}
+//	}
+//	x[n] = (m[n][c]) ? 1 : 0;
+//}
+//int main()
+//{
+//	int n,c;
+//	cin >> n>>c;
+//	int* w = new int[11];
+//	int* v = new int[11];
+//	int* x = new int[11];
+//	for (int i = 1; i <= n; i++)
+//	{
+//		cin >> w[i];
+//	}
+//	for (int i = 1; i <= n; i++)
+//	{
+//		cin >> v[i];
+//	}
+//	int** m = new int* [11];
+//	for (int i = 0; i <= 10; i++)
+//	{
+//		m[i] = new int[11];
+//		memset(m[i], 0, sizeof(int)*10);
+//	}
+//	package(v, w, c, n, m);
+//	cout << endl;
+//	for (int i = n; i>= 1; i--)
+//	{
+//		for (int j = 0; j <= c; j++)
+//		{
+//			cout << m[i][j] << " ";
+//		}
+//		cout << endl;
+//	}
+//	Traceback(m, w, c, n, x);
+//	for (int i = 1; i <= n; i++)
+//		cout << x[i] << " ";
+//}
+int n;
+int x[11];
+int w[11];
+int v[11];
+int y[11];
+int c;
+int cw;
+int cv;
+int bestv;
+int Bound(int i)
+{
+	int rev=cv;
+	while (i <= n)
+	{
+		rev += v[i];
+		i++;
+	}
+	return rev;
+}
+void search(int i)
+{
+	if (i > n)
+	{
+		if(cv>bestv)
+			bestv = cv;
+		for (int k = 1; k <= n; k++)
+			y[k] = x[k];
+		return;
+	}
+	if (cw + w[i] <= c)
+	{
+		x[i] = 1;
+		cw += w[i];
+		cv += v[i];
+		search(i + 1);
+		cw -= w[i];
+		cv -= v[i];
+	}
+	if (Bound(i + 1) > bestv)
+	{
+		x[i] = 0;
+		search(i + 1);
+	}
+}
+int main()
+{
+	cin >> n >> c;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> w[i];
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> v[i];
+	}
+	bestv = 0;
+	cw = cv = 0;
+	search(1);
+	cout << endl;
+	cout << bestv<<endl;
+	cout << "最优选择";
+	for (int t = 1; t <= n; t++)
+		cout << y[t] << " ";
+}
---
+++ 动态规划.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+void MaxtrixChain(int *p,int n,int **m,int **s)
+{
+	for (int i = 0; i <= n; i++)
+		m[i][i] = 0;
+	for (int r = 2; r <= n; r++)//r为链中矩阵个数
+	{
+		for (int i = 1; i <= n - r + 1; i++)
+		{
+			int j = i + r - 1;
+			m[i][j] = m[i + 1][j] + p[i - 1] * p[i] * p[j];//k=i时，m[i][k]=0
+			s[i][j] = i;//此时断开在i
+			for (int k = i + 1; k < j; k++)
+			{
+				int t = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+				if (t < m[i][j])
+				{
+					m[i][j] = t;
+					s[i][j] = k;
+				}
+			}
+		}
+	}
+	
+}
+void Traceback(int i, int j, int** s)
+{
+	if (i == j)
+	{
+		cout << "A" << i;
+		return;
+	}
+	cout << "(";
+	Traceback(i, s[i][j], s);
+	cout << "*";
+	Traceback(s[i][j] + 1, j, s);
+	cout << ")";
+}
+int main()
+{
+	int n;
+	cin >> n;
+	int p[10];
+	for (int i = 0; i <= n; i++)
+	{
+		cin >> p[i];
+	}
+	int** m = new int* [10];
+	int** s = new int* [10];
+	for (int i = 0; i < 10; i++)
+	{
+		m[i] = new int[10];
+		s[i] = new int[10];
+	}
+	MaxtrixChain(p, n, m,s);
+	cout << "最优解: " << endl;
+	Traceback(1, n,s);
+	cout << endl;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = i; j <= n; j++)
+			cout << m[i][j] << " ";
+		cout << endl;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		s[i][i] = 0;
+		for (int j = i; j <= n; j++)
+			cout << s[i][j] << " ";
+		cout << endl;
+	}
+	cout << endl;
+	cout << "最优解: " << m[1][n];
+	
+}
+
+
+//void MaxtrixChain(int* p, int n, int** m,int **sum)
+//{
+//	for (int i = 1; i <= n; i++)
+//		m[i][i] =0;
+//	for (int r = 2; r <= n; r++)//r为链中矩阵个数
+//	{
+//		for (int i = 1; i <= n - r + 1; i++)
+//		{
+//			int j = i + r - 1;
+//			m[i][j] = m[i][i]+m[i + 1][j]+sum[i][j];
+//			for (int k = i + 1; k < j; k++)
+//			{
+//				int t = m[i][k] + m[k + 1][j]+sum[i][j];
+//				if (t < m[i][j])
+//				{
+//					m[i][j] = t;
+//				}
+//			}
+//		}
+//	}
+//
+//}
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int p[10];
+//	for (int i = 1; i <= n; i++)
+//	{
+//		cin >> p[i];
+//	}
+//	int** m = new int* [10];
+//	int** sum = new int* [10];
+//	for (int i = 0; i < 10; i++)
+//	{
+//		m[i] = new int[10];
+//		sum[i] = new int[10];
+//	}
+//	
+//	for (int i = 1; i <= n; i ++ )
+//		sum[i][i] = p[i];
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = i+1; j <= n; j++)
+//		{
+//			sum[i][j] = sum[i][j - 1] + p[j];
+//		}
+//	}
+//	MaxtrixChain(p, n, m,sum);
+//	for (int i = 1; i <= n; i++)
+//	{
+//		for (int j = i; j <= n; j++)
+//			cout << m[i][j] << " ";
+//		cout << endl;
+//	}
+//	
+//	cout << "最优解: " << m[1][n];
+//
+//}
+
